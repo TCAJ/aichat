594b5499c7acda9d413dc7efcd7d80b401939acf
TCAJ <13961982461@163.com>
2026-09-12 12:52:21 +0800

    Add files via upload

---
+++ deepseek_kotlin_20260912_f3bb7f.kt
@@ -0,0 +1,26 @@
+package com.example.aichat
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+
+class MainActivity : ComponentActivity() {
+
+    private val vm: ChatViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(Modifier.fillMaxSize()) {
+                    ChatScreen(vm)
+                }
+            }
+        }
+    }
+}
